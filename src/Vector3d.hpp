e42e473b618deedb046a1f67bbed89a5d677887a
dainbow <suslov9876@gmail.com>
2022-09-20 01:14:31 +0300

    RayTrace first realisation

--- src/Vector3d.hpp
+++ src/Vector3d.hpp
@@ -3,32 +3,36 @@
 #include <cmath>
 #include <iostream>
 
+
+#include "Utilities.hpp"
+
 class Vector3D {
     public:
-        float x_;
-        float y_;
-        float z_;
+        double x_;
+        double y_;
+        double z_;
 
-        float CosBetween(const Vector3D& vector2) const;
+        double CosBetween(const Vector3D& vector2) const;
 
-        float operator*(const Vector3D& vector2) const;
+        double operator*(const Vector3D& vector2) const;
 
-        void Resize(const float newLength);
+        void Resize(const double newLength);
 
-        void operator *=(float scalar);
+        void operator *=(double scalar);
         void operator +=(const Vector3D& vectorToAdd);
         void operator -=(const Vector3D& vectorToSub);
 
-        Vector3D operator*(float scalar) const;
+        Vector3D operator*(double scalar) const;
         Vector3D operator+(const Vector3D& vectorToAdd) const;
         Vector3D operator-(const Vector3D& vectorToSub) const;
 
         Vector3D operator-() const;
 
         void Normalise();
+        bool IsNan() const;
 
-        float LengthSquared() const;
-        float Length() const;
+        double LengthSquared() const;
+        double Length() const;
 };
 
 std::ostream& operator<<(std::ostream& outStream, const Vector3D& curVector);
